28610095f7c8b867936a6f0740e7b6add82aa459
paulj <pauljosch@gmx.de>
2025-03-13 19:08:21 +0100

    init

---
+++ Blazor/Models/WorkContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Blazor.Models;
+
+public class WorkContext : DbContext
+{
+    public WorkContext(DbContextOptions<WorkContext> options) : base(options) { }
+
+    public DbSet<WorkDays> WorkDays { get; set; } // Example entity
+}
