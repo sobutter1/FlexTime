2104118be5c8aefc2b01da9ba91860870b7e60c1
paulj <pauljosch@gmx.de>
2025-03-19 11:58:38 +0100

    init

---
+++ Blazor/Models/WorkDays.cs
@@ -0,0 +1,10 @@
+﻿namespace Blazor.Models;
+
+public class WorkDays
+{
+    public int Id { get; set; }
+    public DateTime FromTime { get; set; }
+    public DateTime ToTime { get; set; }
+    public DateTime BrakeTime { get; set; }
+    public TimeSpan Balance { get; set; }
+}
